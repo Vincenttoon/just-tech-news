e6e83f2d29f03f67f7b39bcb4913b999258181cc
Vincent Toon <vincenttoon22@gmail.com>
2022-10-03 00:09:53 -0500

    password security and verification added

--- README.md
+++ README.md
@@ -4,17 +4,17 @@
 	"username": "Taylor Pooz",
 	"email": "plantmami@email.com",
 	"password": "bookworm1"
-},
+}
 {
 	"username": "The Dean Machine",
 	"email": "redmenace123@email.com",
 	"password": "pencil1"
-},
+}
 {
 	"username": "Clam",
 	"email": "pancakepapi@email.com",
 	"password": "codgod420"
-},
+}
 {
 	"username": "Smithers",
 	"email": "sillyman@email.com",
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -47,6 +47,29 @@ router.post("/", (req, res) => {
     });
 });
 
+router.post("/login", (req, res) => {
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+  User.findOne({
+    where: {
+      email: req.body.email,
+    },
+  }).then((dbUserData) => {
+    if (!dbUserData) {
+      res.status(400).json({ message: "No user with that email address!" });
+      return;
+    }
+
+    // Verify User
+    const validPassword = dbUserData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res.status(400).json({ message: "Incorrect password!" });
+      return;
+    }
+
+    res.json({ user: dbUserData, message: "You are now logged in!" });
+  });
+});
+
 router.put("/:id", (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
 
@@ -55,7 +78,7 @@ router.put("/:id", (req, res) => {
     individualHooks: true,
     where: {
       id: req.params.id,
-    }
+    },
   })
     .then((dbUserData) => {
       if (!dbUserData[0]) {
