8033e72bd1ed8e9e23a02fc15d967f476d3f1907
Vincent Toon <vincenttoon22@gmail.com>
2022-10-10 00:40:17 -0500

    handlebars working and started

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
-const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes');
+router.use('/', homeRoutes);
 
+const apiRoutes = require('./api');
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
